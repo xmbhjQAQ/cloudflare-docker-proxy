8e97526b615aa1c0ad6417946327e905f54d4e82
QAQneko <72352414+xmbhjQAQ@users.noreply.github.com>
2024-08-15 18:09:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -43,6 +45,16 @@ async function handleRequest(request) {
       }
     );
   }
+   
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
@@ -154,14 +166,3 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
   return await fetch(url, { method: "GET", headers: headers });
 }
 
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-}
